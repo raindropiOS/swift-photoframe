67083fc622d89b4423ddb57b322a171ec601ab34
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2023-03-08 14:19:18 +0900

    photoLabel, firstDescription 속성 변경
    viewDidLoad 함수에서 속성이 변경될 수 있도록 함.

--- PhotoFrame/PhotoFrame/ViewController.swift
+++ PhotoFrame/PhotoFrame/ViewController.swift
@@ -14,6 +14,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print(#file, #line, #function, #column)
+        self.photoLabel.text = "에디의 사진액자"
+        photoLabel.sizeToFit()
+        photoLabel.textColor = .white
+        photoLabel.backgroundColor = .orange
+        photoLabel.alpha = 0.5
+        photoLabel.font = UIFont.boldSystemFont(ofSize: 25.0)
+        
+        firstDescription.text = "나의 첫번째 사진앱"
+        firstDescription.font = firstDescription.font.withSize(15)
+        firstDescription.highlightedTextColor = .orange
+        firstDescription.isHighlighted = true
     }
 
 }
