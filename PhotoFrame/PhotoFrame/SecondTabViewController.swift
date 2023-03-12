cf0baef55990a761cf22c2c74a377a3cf1e7f24c
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2023-03-13 02:43:42 +0900

    SecondTab 랜덤이미지 추가
    randomImage() 함수 추가

--- PhotoFrame/PhotoFrame/SecondTabViewController.swift
+++ PhotoFrame/PhotoFrame/SecondTabViewController.swift
@@ -12,9 +12,30 @@ class SecondTabViewController: UIViewController {
     @IBOutlet weak var photoImageView: UIImageView!
     
     @IBAction func nextImageButtonTouched(_ sender: Any) {
+        self.photoImageView.image = randomImage()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+}
+
+extension SecondTabViewController {
+    func randomImage() -> UIImage {
+        let randomImageNumber = (1...22).randomElement()!
+        
+        let randomImageNumberString: String
+        if randomImageNumber < 10 {
+            randomImageNumberString = "0" + String(randomImageNumber) + ".jpg"
+        } else {
+            randomImageNumberString = String(randomImageNumber) + ".jpg"
+        }
+        
+        if let image = UIImage(named: randomImageNumberString) {
+            return image
+        }
+        
+        print("error function : randomImage")
+        return UIImage(named: "01.jpg")!
     }
 }
