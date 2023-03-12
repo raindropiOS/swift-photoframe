635e1bd0ae3c9ea3dc4f23122f6729691fce5702
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2023-03-13 02:30:57 +0900

    SecondTabViewController 추가
    이미지 뷰, 포토 라벨, 다음 버튼 추가
    이미지 파일 추가

---
+++ PhotoFrame/PhotoFrame/SecondTabViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondTabViewController.swift
+//  PhotoFrame
+//
+//  Created by 에디 on 2023/03/13.
+//
+
+import UIKit
+
+class SecondTabViewController: UIViewController {
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    @IBAction func nextImageButtonTouched(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
