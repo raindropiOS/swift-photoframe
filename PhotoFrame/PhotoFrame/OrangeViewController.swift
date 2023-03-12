6034440db666fa816e4e5cbf779ad8a8c46cac76
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2023-03-12 23:44:26 +0900

    OrangeViewController 추가
    닫기 버튼 기능 추가
    화면 관련 콜백 함수 추가

---
+++ PhotoFrame/PhotoFrame/OrangeViewController.swift
@@ -0,0 +1,39 @@
+//
+//  GreenViewController.swift
+//  PhotoFrame
+//
+//  Created by 에디 on 2023/03/12.
+//
+
+import UIKit
+
+class OrangeViewController: UIViewController {
+
+    @IBAction func closeButtonTouched(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("viewWillAppear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("viewDidAppear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("viewWillDisappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("viewDidDisappear")
+    }
+}
